f67459ff9f45e6143da685cb1299d30a3b9f0be1
EmilienV <emilienvolkaert@gmail.com>
2023-05-04 12:29:31 +0200

    filter 1st step done

--- assets/js/filter.js
+++ assets/js/filter.js
@@ -1,3 +1,36 @@
-let btnToDo = document.getElementsByClassName("filter-toDo");
-let btnDoing = document.getElementsByClassName("filter-doing");
-let btnDone = document.getElementsByClassName("filter-done");
+export function filter() {
+  const btnToDo = document.getElementsByClassName("filter-toDo");
+  const btnDoing = document.getElementsByClassName("filter-doing");
+  const btnDone = document.getElementsByClassName("filter-done");
+  const project = document.getElementsByClassName("project");
+
+  btnToDo.addEventListener("click", () => {
+    project.forEach((projectItem) => {
+      if (projectItem.classList("toDo")) {
+        project.style.display = "block";
+      } else {
+        project.style.display = "none";
+      }
+    });
+  });
+
+  btnDoing.addEventListener("click", () => {
+    project.forEach((projectItem) => {
+      if (projectItem.classList("doing")) {
+        project.style.display = "block";
+      } else {
+        project.style.display = "none";
+      }
+    });
+  });
+
+  btnDone.addEventListener("click", () => {
+    project.forEach((projectItem) => {
+      if (projectItem.classList("done")) {
+        project.style.display = "block";
+      } else {
+        project.style.display = "none";
+      }
+    });
+  });
+}
