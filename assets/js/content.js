ff8810cee6a384add11045b2520c94e2008b0aa0
Anthony <denayeranthony1782@gmail.com>
2023-05-05 15:19:50 +0200

    mwip mobile design

--- assets/js/content.js
+++ assets/js/content.js
@@ -1,6 +1,5 @@
+import { daysLeft } from "./date.js";
 import { getItemStorage } from "./getItemAndStore.js";
-import { daysLeft} from "./date.js";
-
 export function createContent() {
   let plannerContent = document.querySelector(".planner-content");
   plannerContent.innerHTML = "";
@@ -8,13 +7,12 @@ export function createContent() {
   myArray.forEach((element) => {
     let name = element.name;
     let description = element.description;
-    let date = element.date;
-    // let daysLeft = daysLeft(date);
+    let daysLeft = element.daysLeft;
     let content = `<div class="planner-content-global">
     <section class="content-index"></section>
     <section class="content-name">${name}</section>
     <section class="content-description">${description}</section>
-    <section class="content-date">${date}</section>
+    <section class="content-date">${daysLeft}</section>
     <section class="content-status"><button class="button-status"></button></section>
   </div>
   `;
