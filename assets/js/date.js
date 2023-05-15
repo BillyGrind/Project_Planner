f941a71969d6a071b4c16a300a235fcd33d2e349
Volkaert Emilien <emilienvolkaert@gmail.com>
2023-05-15 09:49:46 +0200

    Revert "Development"

--- assets/js/date.js
+++
@@ -1,8 +0,0 @@
-export function daysLeft(deadline) {
-  let now = new Date();
-  let diff = deadline.getTime() - now.getTime();
-  let daysLeft = Math.floor(diff / (1000 * 60 * 60 * 24));
-  console.log(daysLeft);
-  return daysLeft.toLocaleString();
-  
-}
--- assets/js/toggleButton.js
+++
@@ -1,6 +0,0 @@
-export function toggleButton() {
-  let toggleButton = document.getElementsByTagName("button-status");
-  toggleButton.forEach((element) => {
-    console.log(element);
-  });
-}
