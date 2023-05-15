c5fd308a47d4b8b35ca1f87206e695000f1d6ea4
EmilienV <emilienvolkaert@gmail.com>
2023-05-15 09:43:24 +0200

    début de semaine

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
+import { changeStatus, btnStatus } from "./buttons.js";
 import { createAddContainer } from "./add.js";
 import { createContent } from "./content.js";
 import { filter } from "./filter.js";
 import { orderByName } from "./filter.js";
-import { changeStatus, btnStatus } from "./buttons.js";
 
 createAddContainer();
 window.onload = createContent();
